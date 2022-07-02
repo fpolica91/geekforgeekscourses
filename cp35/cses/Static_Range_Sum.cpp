b7a02bbb2d5cb3f9012c2de5556dfe447d8f1ac3
king kai <fabriciopolicarpo0@gmail.com>
2022-07-02 16:30:11 -0400

    static range sum

---
+++ cp35/cses/Static_Range_Sum.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define watch(x) cout << (#x) << " is " << (x) << "\n"
+#define watch2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << "\n"
+using ll = long long int;
+vector<ll> prefix;
+
+// static range sum
+
+int main(){
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+   int n, q;
+   cin>> n >> q;
+   prefix.resize(n + 1);
+   prefix[n + 1] = 0;
+   
+   for(int i = 1; i < n + 1; i++){
+      int val;
+      cin>> val;
+      prefix[i] = prefix[i - 1]+ val;
+   }
+
+    for(int i = 1; i <= n; i++){
+      int l, r;
+      cin >> l >> r;
+      cout << prefix[r] - prefix[l] << endl;
+    }  
+}
+
