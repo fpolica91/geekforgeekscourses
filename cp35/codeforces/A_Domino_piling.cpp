b17d2e06b8ec55c2ea61104ebec8a66e4deb8dd2
king kai <fabriciopolicarpo0@gmail.com>
2022-07-09 18:28:41 -0400

    domino pilling

---
+++ cp35/codeforces/A_Domino_piling.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define watch(x) cout << (#x) << " is " << (x) << "\n"
+#define watch2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << "\n"
+int n, m;
+
+int main(){
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+   int n, m;
+   cin >> n >> m;
+   int rect = n * m;
+   int res = rect/2;
+   cout << res;
+}
+
