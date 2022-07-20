6499f53d8ce0325286e3c7b1565205e6d00f53a4
king kai <fabriciopolicarpo0@gmail.com>
2022-07-19 21:25:46 -0400

    linked lists

--- cp35/cpp/linkedLists/detect_cycle.cpp
+++ cp35/cpp/linkedLists/detect_cycle.cpp
@@ -22,18 +22,22 @@ ListNode *detect_cycle(ListNode *head){
   while(fast and fast->next){
     slow = slow->next;
     fast = fast->next->next;
+    // meeting point of slow and fast pointer, at this point they will be k steps away from the start of the loop
     if(slow == fast){
       break;
     }
   }
+  // if there is no cycle, fast pointer will reach the end of the list
   if(!fast or !fast->next){
     return NULL;
   }
   slow = head;
+  // the distance from start of loop to the meeting point is k-n steps where n is the length of the loop
   while(slow != fast){
     slow = slow->next;
     fast = fast->next;
   }
+  // slow pointer is at the start of the loop
   return slow;
 }
 
